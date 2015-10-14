d2bba3174ad060b29eea453e111d8507d0050b54
Keyu-Chen <keyuchenstuff@gmail.com>
2015-10-14 10:24:27 -0500

    hourUp and minuteUp
    We programmed the arduino so that it cn understand the hour up and
    minute up commands and dispay the numbers on the sevseg display. Now, we
    can use the buttons to manually control the clock.

--- digital_clock.ino
+++ digital_clock.ino
@@ -1,5 +1,3 @@
-// Arduino Clock
-//
 
 #include "SevSeg.h"
 
@@ -7,112 +5,94 @@
 #define d2 12
 #define d3 11
 #define d4 10
-#define dp 9
-#define g 8
+#define a A0
+#define b A1
+#define c A2
+#define d A3
+#define e A4
 #define f 7
-#define e 6
-#define d 5
-#define c 4
-#define b 3
-#define a 2
+#define g 6
+#define dp 9
+#define HOUR_BUTTON 2
+#define MINUTE_BUTTON 3
+#define TICK_LENGTH 250
+
+SevSeg sevseg;
+
+int minute = 0;
+int hour = 1;
+int ticks = 0;
+unsigned long previousMillis = 0;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(d1, OUTPUT);
-  pinMode(a, OUTPUT);
-  pinMode(f, OUTPUT);
-  pinMode(d2, OUTPUT);
-  pinMode(d3, OUTPUT);
-  pinMode(b, OUTPUT);
-  pinMode(e, OUTPUT);
-  pinMode(d, OUTPUT);
-  pinMode(dp, OUTPUT);
-  pinMode(c, OUTPUT);
-  pinMode(g, OUTPUT);
-  pinMode(d4, OUTPUT);
+  Serial.begin(9600);
+  Serial.println("Bootup");
+  pinMode(HOUR_BUTTON, INPUT);
+  pinMode(MINUTE_BUTTON, INPUT);
+  
+  byte numDigits = 4;
+  byte digitPins[] = {d1, d2, d3, d4};
+  byte segmentPins[] = {a, b, c, d, e, f, g, dp};
+  sevseg.begin(COMMON_CATHODE, numDigits, digitPins, segmentPins);
+  
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(dp, HIGH);
-  digitalWrite(a, LOW);
-  digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
-  digitalWrite(d, LOW);
-  digitalWrite(e, LOW);
-  digitalWrite(f, LOW);
-  digitalWrite(g, LOW);
-//  digitalWrite(d1, HIGH);
-  digitalWrite(d1, LOW);
-  digitalWrite(d2, HIGH);
-//  digitalWrite(d2, LOW);
-  digitalWrite(d3, HIGH);
-//  digitalWrite(d3, LOW);
-  digitalWrite(d4, HIGH);
-//  digitalWrite(d4, LOW);
-  delay(1000);
-  digitalWrite(a, HIGH);
-  digitalWrite(b, HIGH);
-  digitalWrite(c, LOW);
-  digitalWrite(d, HIGH);
-  digitalWrite(e, HIGH);
-  digitalWrite(f, LOW);
-  digitalWrite(g, HIGH);
-//  digitalWrite(d1, HIGH);
-  digitalWrite(d1, LOW);
-  digitalWrite(d2, HIGH);
-//  digitalWrite(d2, LOW);
-  digitalWrite(d3, HIGH);
-//  digitalWrite(d3, LOW);
-  digitalWrite(d4, HIGH);
-//  digitalWrite(d4, LOW);
-  delay(1000);
-  digitalWrite(a, HIGH);
-  digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
-  digitalWrite(d, HIGH);
-  digitalWrite(e, LOW);
-  digitalWrite(f, LOW);
-  digitalWrite(g, HIGH);
-//  digitalWrite(d1, HIGH);
-  digitalWrite(d1, LOW);
-  digitalWrite(d2, HIGH);
-//  digitalWrite(d2, LOW);
-  digitalWrite(d3, HIGH);
-//  digitalWrite(d3, LOW);
-  digitalWrite(d4, HIGH);
-//  digitalWrite(d4, LOW);
-  delay(1000);
-  digitalWrite(a, LOW);
-  digitalWrite(b, HIGH);
-  digitalWrite(c, HIGH);
-  digitalWrite(d, LOW);
-  digitalWrite(e, LOW);
-  digitalWrite(f, HIGH);
-  digitalWrite(g, HIGH);
-//  digitalWrite(d1, HIGH);
-  digitalWrite(d1, LOW);
-  digitalWrite(d2, HIGH);
-//  digitalWrite(d2, LOW);
-  digitalWrite(d3, HIGH);
-//  digitalWrite(d3, LOW);
-  digitalWrite(d4, HIGH);
-//  digitalWrite(d4, LOW);
-  delay(1000);
-  digitalWrite(a, HIGH);
-  digitalWrite(b, LOW);
-  digitalWrite(c, HIGH);
-  digitalWrite(d, HIGH);
-  digitalWrite(e, LOW);
-  digitalWrite(f, HIGH);
-  digitalWrite(g, HIGH);
-//  digitalWrite(d1, HIGH);
-  digitalWrite(d1, LOW);
-  digitalWrite(d2, HIGH);
-//  digitalWrite(d2, LOW);
-  digitalWrite(d3, HIGH);
-//  digitalWrite(d3, LOW);
-  digitalWrite(d4, HIGH);
-//  digitalWrite(d4, LOW);
-  delay(1000);
+  checkTick();
+  sevseg.setNumber(hour * 100 + minute, 3);
+  sevseg.refreshDisplay(); 
 }
+
+void checkTick() {
+   unsigned long currentMillis = millis();
+ 
+   // How much time has passed, accounting for rollover with subtraction!
+   if ((unsigned long)(currentMillis - previousMillis) >= TICK_LENGTH) {
+      // It's time to do something!
+      tick();
+ 
+      // Use the snapshot to set track time until next event
+      previousMillis = currentMillis;
+   }
+}
+
+void tick() {
+  Serial.println("TICK!");
+  ticks = ticks + 1;
+  readHourButton();
+  readMinuteButton();
+}
+
+void readHourButton() {
+  if (digitalRead(HOUR_BUTTON) == HIGH) {
+    hourUp();
+  }
+}
+
+void readMinuteButton() {
+  if (digitalRead(MINUTE_BUTTON) == HIGH) {
+    minuteUp();
+  }
+}
+
+void hourUp() {
+  Serial.print("Hour up: ");
+  Serial.println(hour);
+  hour = hour + 1;
+  if (hour > 12) {
+    hour = 1;
+  }
+}
+
+void minuteUp() {
+  Serial.print("Minute up: ");
+  Serial.println(minute);
+  minute = minute + 1;
+  if (minute > 59) {
+    minute = 0;
+    hourUp;
+  }
+}
+
